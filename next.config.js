2a5d8c0444fd69913f8c79c8272438f96ed641dc
Adil Sezer <sezeradil@gmail.com>
2023-08-18 13:12:39 +0100

    WIP

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: "/garbanu-klinika",
+  assetPrefix: "/garbanu-klinika/",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
