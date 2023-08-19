4560d105d831aa75ba68afaaef9c2cfd8174b6d8
Adil Sezer <sezeradil@gmail.com>
2023-08-19 18:34:38 +0100

    WIP

--- app/components/Slide/Slide.tsx
+++ app/components/Slide/Slide.tsx
@@ -27,9 +27,13 @@ const Slide: React.FC<SlideProps> = ({
     className="h-screen w-full flex flex-col items-center justify-center bg-cover bg-center p-4 md:p-0"
     style={{ backgroundImage: image ? `url(${image})` : undefined }}
   >
-    <div className={contents ? "" : "mt-96"}>
+    <div
+      className={`flex flex-col items-center justify-center h-screen ${
+        contents ? "" : "mt-96"
+      }`}
+    >
       <h1
-        className={`text-4xl md:text-8xl font-extrabold	${
+        className={`text-4xl md:text-8xl font-extrabold ${
           contents ? "text-primary" : "text-secondary shadow-text"
         }`}
       >
@@ -42,15 +46,20 @@ const Slide: React.FC<SlideProps> = ({
         </p>
       )}
       {contents && (
-        <div className="flex flex-wrap justify-center space-x-2 m-4">
+        <div className="flex flex-wrap justify-center space-x-2 m-2 md:m-4">
           {contents.slice(0, 5).map((contentItem, index) => (
-            <div key={contentItem.image} className="relative m-2">
-              <div className="w-60 h-80 shadow-lg relative overflow-hidden mb-4 rounded-lg">
+            <div
+              key={contentItem.serviceName}
+              className="relative m-1 md:m-2 w-1/3 md:w-60"
+            >
+              <div className="h-40 md:h-80 shadow-lg relative overflow-hidden mb-4 rounded-lg">
                 <Image
                   src={contentItem.image}
                   alt={`Product ${index + 1}`}
-                  layout="fill"
-                  objectFit="cover"
+                  className="object-cover w-full h-full"
+                  layout="responsive"
+                  width={3}
+                  height={4}
                 />
                 {/* ... rest of the code ... */}
               </div>
