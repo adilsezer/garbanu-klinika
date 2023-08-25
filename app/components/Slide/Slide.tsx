e4dfebfbc53a608fb73b994d74b168efb66334d5
Adil Sezer <sezeradil@gmail.com>
2023-08-25 01:50:55 +0100

    Refactorings on Slide component

--- app/components/Slide/Slide.tsx
+++ app/components/Slide/Slide.tsx
@@ -3,22 +3,56 @@ import Link from "next/link";
 import Image from "next/image";
 import { SlideType } from "./SlideType";
 
+interface ContentItem {
+  contentName: string;
+  contentDesc?: string;
+  contentPrice?: string;
+  contentImage?: string;
+}
+
+interface Button {
+  label: string;
+  action: string;
+}
+
 interface SlideProps {
   title: string;
   type: SlideType;
   subtitle?: string;
   image?: string;
-  contents?: {
-    contentName: string;
-    contentDesc?: string;
-    contentPrice?: string;
-    contentImage?: string;
-  }[];
-  buttons?: {
-    label: string;
-    action: string;
-  }[];
+  contents?: ContentItem[];
+  buttons?: Button[];
 }
+
+const ContentCard: React.FC<{
+  item: ContentItem;
+  type: SlideType;
+  index: number;
+}> = ({ item, type, index }) => {
+  const card = (
+    <div className="shadow-lg relative overflow-hidden rounded-lg h-full w-full">
+      {item.contentImage && (
+        <Image
+          fill
+          src={item.contentImage}
+          alt={`Product ${index + 1}`}
+          style={{ objectFit: "cover" }}
+        />
+      )}
+      <div className="absolute bottom-0 left-0 right-0 bg-primary-opacity-30 font-semibold p-1 md:p-2">
+        <p className="text-xs md:text-base text-white">{item.contentName}</p>
+        <p className="text-xs md:text-base text-white">{item.contentDesc}</p>
+        <p className="text-xs md:text-base text-white">{item.contentPrice}</p>
+      </div>
+    </div>
+  );
+
+  if (type === SlideType.Product && item.contentDesc) {
+    return <Link href={`/${item.contentDesc.replace(" ", "")}`}>{card}</Link>;
+  }
+
+  return card;
+};
 
 const Slide: React.FC<SlideProps> = ({
   title,
@@ -28,53 +62,6 @@ const Slide: React.FC<SlideProps> = ({
   contents,
   buttons,
 }) => {
-  const renderContentCard = (
-    contentItem: {
-      contentName: string;
-      contentDesc?: string;
-      contentPrice?: string;
-      contentImage?: string;
-    },
-    index: number
-  ) => {
-    const card = (
-      <div className="shadow-lg relative overflow-hidden rounded-lg h-full w-full">
-        {contentItem.contentImage && (
-          <Image
-            fill
-            src={contentItem.contentImage}
-            alt={`Product ${index + 1}`}
-            style={{ objectFit: "cover" }}
-          />
-        )}
-        <div className="absolute bottom-0 left-0 right-0 bg-primary-opacity-30 font-semibold p-1 md:p-2">
-          <p className="text-xs md:text-base text-white">
-            {contentItem.contentName}
-          </p>
-          <p className="text-xs md:text-base text-white">
-            {contentItem.contentDesc}
-          </p>
-          <p className="text-xs md:text-base text-white">
-            {contentItem.contentPrice}
-          </p>
-        </div>
-      </div>
-    );
-
-    if (type === SlideType.Product && contentItem.contentDesc) {
-      return (
-        <Link
-          key={contentItem.contentName}
-          href={`/${contentItem.contentDesc.replace(" ", "")}`}
-        >
-          {card}
-        </Link>
-      );
-    }
-
-    return card;
-  };
-
   return (
     <div
       className="h-full w-full flex flex-col items-center justify-center bg-cover bg-center p-4 overflow-hidden"
@@ -102,14 +89,14 @@ const Slide: React.FC<SlideProps> = ({
         {(type === SlideType.Product || type === SlideType.Service) &&
           contents && (
             <div className="flex flex-wrap justify-center my-2 md:my-8 w-screen">
-              {contents.slice(0, 4).map((contentItem, index) => (
+              {contents.slice(0, 4).map((item, index) => (
                 <div
-                  key={contentItem.contentName}
+                  key={item.contentName}
                   className={`relative mx-2 my-2 w-2/5 h-24 md:w-1/6 md:h-60 ${
                     type === SlideType.Product ? "cursor-pointer" : ""
                   }`}
                 >
-                  {renderContentCard(contentItem, index)}
+                  <ContentCard item={item} type={type} index={index} />
                 </div>
               ))}
             </div>
@@ -117,18 +104,18 @@ const Slide: React.FC<SlideProps> = ({
 
         {type === SlideType.TextCard && contents && (
           <div className="flex flex-wrap justify-center my-2 md:my-10">
-            {contents.slice(0, 4).map((contentItem, index) => (
+            {contents.slice(0, 4).map((item, index) => (
               <div
-                key={contentItem.contentName}
+                key={item.contentName}
                 className="relative m-2 md:m-4 w-11/12 md:w-1/6 h-auto bg-gradient-to-br from-primary to-primary-opacity-50 rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105"
               >
                 <div className="p-4">
                   <p className="font-bold text-white text-center mb-1">
-                    {contentItem.contentName}
+                    {item.contentName}
                   </p>
-                  {contentItem.contentDesc && (
+                  {item.contentDesc && (
                     <p className="text-white text-xs md:text-sm leading-relaxed text-justify mt-2">
-                      {contentItem.contentDesc}
+                      {item.contentDesc}
                     </p>
                   )}
                 </div>
