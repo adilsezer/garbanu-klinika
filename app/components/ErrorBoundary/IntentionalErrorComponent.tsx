eba8462b9af2ef6b17c374cc9373ce95afc8bcbc
Adil Sezer <sezeradil@gmail.com>
2023-08-25 02:19:43 +0100

    Error Boundary Improvements

--- app/components/ErrorBoundary/IntentionalErrorComponent.tsx
+++
@@ -1,9 +0,0 @@
-// IntentionalErrorComponent.tsx
-import React from "react";
-
-const IntentionalErrorComponent: React.FC = () => {
-  throw new Error("This is an intentional error for testing purposes.");
-  return <div>Should not reach here</div>;
-};
-
-export default IntentionalErrorComponent;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
-import IntentionalErrorComponent from "./components/ErrorBoundary/IntentionalErrorComponent";
 import FullPageSlider from "./components/Slide/FullPageSlider";
 
 export default function Home() {
   return (
     <div>
       <ErrorBoundary>
-        <IntentionalErrorComponent />
         <FullPageSlider />
       </ErrorBoundary>
     </div>
