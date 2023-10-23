49ebed51912b0325df368a716ddbd8df1decc68d
Adil Sezer <sezeradil@gmail.com>
2023-10-23 02:01:13 +0100

    Adjustments on promobanner component

--- app/(routes)/root/components/PromotionalBanner.tsx
+++ app/(routes)/root/components/PromotionalBanner.tsx
@@ -1,4 +1,3 @@
-// components/PromotionalBanner.tsx
 "use client";
 import React from "react";
 import { useFirestoreData } from "@hooks/useFirestoreData";
@@ -8,14 +7,11 @@ interface PromotionalBannerProps {
 }
 
 const PromotionalBanner: React.FC<PromotionalBannerProps> = ({ docPath }) => {
-  if (!docPath) {
-    return null;
-  }
+  const { data, loading, error } = useFirestoreData(docPath || "");
 
-  const { data, loading, error } = useFirestoreData(docPath);
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  if (!docPath || !data) return null;
+  else if (loading) return <div>Loading...</div>;
+  else if (error) return <div>Error: {error.message}</div>;
 
   return <div className="bg-black text-white text-center py-2">{data}</div>;
 };
