94317bda9a4eb24c3591630adbf2a74cf3388cd0
Adil Sezer <sezeradil@gmail.com>
2023-10-23 01:42:07 +0100

    Added "use client"

--- hooks/useFirestoreData.ts
+++ hooks/useFirestoreData.ts
@@ -1,4 +1,5 @@
 // hooks/useFirestoreData.ts
+"use client";
 import { useState, useEffect } from "react";
 import db from "../lib/firebase/firebaseInit";
 import { doc, getDoc } from "firebase/firestore";
