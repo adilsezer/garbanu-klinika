d62ef775030fb98f2b5a43da13c3620d59b271d6
Adil Sezer <sezeradil@gmail.com>
2023-11-01 23:28:21 +0000

    Fixing image issue in coming soon component

--- app/[locale]/coming-soon/page.tsx
+++ app/[locale]/coming-soon/page.tsx
@@ -1,20 +1,57 @@
-// pages/coming-soon.tsx
 import React from "react";
+import Image from "next/image";
+
+// Constants
+const CONTENT = [
+  {
+    title: "Netrukus pasirodys",
+    description:
+      "Mūsų svetainė šiuo metu kuriama. Netrukus pasirodysime su nauja nuostabia svetaine.",
+  },
+  {
+    title: "Coming Soon",
+    description:
+      "Our website is under construction. We will be here soon with our new awesome site.",
+  },
+];
+
+// Reusable Content Block
+type ContentBlockProps = {
+  title: string;
+  description: string;
+};
+
+const ContentBlock = ({ title, description }: ContentBlockProps) => (
+  <div>
+    <h2 className="text-2xl font-bold">{title}</h2>
+    <p className="text-lg">{description}</p>
+  </div>
+);
 
 const ComingSoon = () => {
+  const textStyle = "bg-first bg-opacity-50 p-4 rounded-md z-10";
+
   return (
-    <div>
-      <h1>Netrukus pasirodys</h1>
-      <p>
-        Mūsų svetainė šiuo metu kuriama. Netrukus pasirodysime su nauja
-        nuostabia svetaine.
-      </p>
-      <hr />
-      <h1>Coming Soon</h1>
-      <p>
-        Our website is under construction. We'll be here soon with our new
-        awesome site.
-      </p>
+    <div className="relative h-screen flex items-center justify-center">
+      {/* Background Image */}
+      <Image
+        src="/headers/curly-hair-women-banner.jpg"
+        layout="fill"
+        objectFit="cover"
+        priority={true}
+        alt="Background Image"
+        className="z-0"
+      />
+
+      {/* Text Content */}
+      <div className={textStyle}>
+        {CONTENT.map((block, index) => (
+          <React.Fragment key={index}>
+            <ContentBlock title={block.title} description={block.description} />
+            {index !== CONTENT.length - 1 && <hr className="my-4" />}
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 };
