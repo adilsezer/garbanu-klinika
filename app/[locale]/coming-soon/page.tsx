369f991946457adc16cc99de51cc810236c3c3e0
Adil Sezer <sezeradil@gmail.com>
2023-11-01 23:08:13 +0000

    Setting up "Coming Soon" page on Vercel

---
+++ app/[locale]/coming-soon/page.tsx
@@ -0,0 +1,22 @@
+// pages/coming-soon.tsx
+import React from "react";
+
+const ComingSoon = () => {
+  return (
+    <div>
+      <h1>Netrukus pasirodys</h1>
+      <p>
+        Mūsų svetainė šiuo metu kuriama. Netrukus pasirodysime su nauja
+        nuostabia svetaine.
+      </p>
+      <hr />
+      <h1>Coming Soon</h1>
+      <p>
+        Our website is under construction. We'll be here soon with our new
+        awesome site.
+      </p>
+    </div>
+  );
+};
+
+export default ComingSoon;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    if (process.env.VERCEL_ENV === "production") {
+      return [
+        {
+          source: "/((?!coming-soon).*)",
+          destination: "/coming-soon",
+        },
+      ];
+    }
+    // No rewrites are needed for non-production environments
+    return [];
+  },
 };
 
 module.exports = nextConfig;
