f433482a891ab1d319378416921ed15204d8beff
Adil Sezer <sezeradil@gmail.com>
2023-11-02 01:10:38 +0000

    Testing vercel env

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import PromotionalBanner from "./root/components/PromotionalBanner";
 import Header from "./root/components/Header";
 
+const logVercelEnv = () => {
+  console.log(process.env.VERCEL_ENV);
+};
+
 const MainPage: React.FC = () => {
+  logVercelEnv();
+
   return (
     <main>
-      console.log(process.env.VERCEL_ENV);
       <PromotionalBanner />
       <Header />
     </main>
