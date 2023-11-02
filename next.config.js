288db7547b641932450fe39add42fc7f31b87ee3
Adil Sezer <sezeradil@gmail.com>
2023-11-02 02:11:29 +0000

    reverting vercel env

--- next.config.js
+++ next.config.js
@@ -3,23 +3,6 @@
 
 const nextConfig = {
   reactStrictMode: true,
-
-  async redirects() {
-    // Only redirect in production environment on Vercel
-    if (process.env.VERCEL_ENV === "production") {
-      return [
-        {
-          source: "/((?!coming-soon).*)", // match all routes except for '/coming-soon'
-          destination: "/coming-soon", // redirect to the 'Coming Soon' page
-          permanent: false, // this is not a permanent redirect
-          basePath: false, // if you're using a custom basePath, set this to `false`
-        },
-      ];
-    }
-
-    // No redirects in other environments (preview or development on Vercel)
-    return [];
-  },
 };
 
 module.exports = nextConfig;
