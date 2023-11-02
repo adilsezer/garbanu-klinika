5516386001a12a6de17f5ea775986af7537b4b80
Adil Sezer <sezeradil@gmail.com>
2023-11-02 23:16:33 +0000

    Created middleware for redirection

--- next.config.js
+++ next.config.js
@@ -1,25 +1,8 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
   reactStrictMode: true,
-
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        // Assuming the "Coming Soon" page is at the root
-        destination: "/",
-        // Only redirect if the path is not already the root
-        has: [
-          {
-            type: "pathname",
-            value: "/",
-          },
-        ],
-        permanent: false, // Use false unless you're sure it's a permanent redirect
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
