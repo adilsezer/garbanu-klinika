2b7260805be6299fd745fe804b41b28a06ecd71e
Adil Sezer <sezeradil@gmail.com>
2023-11-04 20:05:07 +0000

    Adjusting comments

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,13 +1,10 @@
 // layout.tsx file to define the layout for all pages in the app
-
-// Importing necessary libraries and configurations
 import "./globals.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
 import { ReactNode } from "react";
-import { locales } from "@/config/i18nconfig";
 import { Analytics } from "@vercel/analytics/react";
 
 // Integrating Roboto font from RootLayout
