ae42313eecc9fdae22d6e88a6cb36f0c85684e41
sezerad <sezeradil@gmail.com>
2023-11-20 20:14:29 +0000

    Fixederror in ProductTypeCards components

--- app/[locale]/root/components/Header/ProductTypePanel/ProductTypeCards.tsx
+++ app/[locale]/root/components/Header/ProductTypePanel/ProductTypeCards.tsx
@@ -20,23 +20,26 @@ const ProductTypeCards: React.FC<ProductTypeCardsProps> = ({
   return (
     <div className="flex flex-wrap justify-center gap-4 md:gap-8 my-4 md:my-8">
       {productTypes.map((product) => (
-        <div className="flex flex-col relative rounded-lg shadow-md overflow-hidden h-40 md:h-80 w-[45%] md:w-[22%] cursor-pointer">
-          <Link key={product.id} href={product.url} passHref>
-            <Image
-              src={product.imageUrl}
-              alt={product.buttonText}
-              fill
-              className="object-cover"
-              sizes="(max-width: 639px) 100vw, (max-width: 767px) 50vw, (max-width: 1023px) 33vw, 25vw"
-            />
-            <div className="absolute inset-0 bg-black bg-opacity-10"></div>
-            <div className="absolute bottom-4 left-0 w-full text-center">
-              <span className="inline-block bg-secondary w-[85%] text-l text-primary font-semibold py-2 px-8 rounded-xl">
-                {product.buttonText}
-              </span>
-            </div>
-          </Link>
-        </div>
+        <Link
+          href={product.url}
+          passHref
+          key={product.id}
+          className="flex flex-col relative rounded-lg shadow-md overflow-hidden h-40 md:h-80 w-[45%] md:w-[22%] cursor-pointer"
+        >
+          <Image
+            src={product.imageUrl}
+            alt={product.buttonText}
+            fill
+            className="object-cover"
+            sizes="(max-width: 639px) 100vw, (max-width: 767px) 50vw, (max-width: 1023px) 33vw, 25vw"
+          />
+          <div className="absolute inset-0 bg-black bg-opacity-10"></div>
+          <div className="absolute bottom-4 left-0 w-full text-center">
+            <span className="inline-block bg-secondary w-[85%] text-l text-primary font-semibold py-2 px-8 rounded-xl">
+              {product.buttonText}
+            </span>
+          </div>
+        </Link>
       ))}
     </div>
   );
