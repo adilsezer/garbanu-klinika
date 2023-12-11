adf87a4292582feaa22781f1ad343e7daedc805b
sezerad <sezeradil@gmail.com>
2023-12-11 03:05:30 +0000

    moved global.css and daisy ui adj

--- app/[locale]/root/components/HomePageBanner/HomePageBanner.tsx
+++ app/[locale]/root/components/HomePageBanner/HomePageBanner.tsx
@@ -16,10 +16,10 @@ export default function HomePageBanner() {
         className="w-full h-auto object-cover object-[80%_center]"
         priority
       />
-      <button className="btn btn-secondary hover:bg-tertiary absolute top-1/3 left-4 md:left-20 w-40 md:w-60 px-8">
+      <button className="btn btn-secondary hover:bg-tertiary absolute top-[30%] left-4 md:left-20 w-40 md:w-60 px-8">
         {t("shop")}
       </button>
-      <button className="btn btn-secondary hover:bg-tertiary absolute top-1/2 left-4 md:left-20 w-40 md:w-60 px-8 mt-4">
+      <button className="btn btn-secondary hover:bg-tertiary absolute top-[50%] left-4 md:left-20 w-40 md:w-60 px-8">
         {t("scheduleAnAppointment")}
       </button>
     </div>
